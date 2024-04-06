6a10827abfa06f358cb56f9fe29b68fece01ee8b
Bamlak11 <77420100+Bamlak11@users.noreply.github.com>
2024-04-06 17:43:45 -0500

    completed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiRestaurant } from "react-icons/bi";
 import { Link } from "react-scroll";
 import Button from "./Button";
+import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineMenuFold } from "react-icons/ai";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  const handleChange = () => {
+    setMenu(!menu);
+  };
+
+  const closeMenu = () => {
+    setMenu(false);
+  };
+
   return (
     <div className=" fixed w-full">
       <div>
         <div className=" flex flex-row justify-between p-5 md:px-32 px-5 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
           <div className=" flex flex-row items-center cursor-pointer">
             <span>
-              <BiRestaurant size={32}/>
+              <BiRestaurant size={32} />
             </span>
             <h1>ExpressEats</h1>
           </div>
           <nav className="hidden md:flex flex-row items-center text-lg font-medium gap-8">
-          <Link
+            <Link
               to="home"
               spy={true}
               smooth={true}
@@ -26,7 +38,7 @@ const Navbar = () => {
             </Link>
 
             <Link
-              to="home"
+              to="menu"
               spy={true}
               smooth={true}
               duration={500}
@@ -36,7 +48,7 @@ const Navbar = () => {
             </Link>
 
             <Link
-              to="home"
+              to="about"
               spy={true}
               smooth={true}
               duration={500}
@@ -46,7 +58,7 @@ const Navbar = () => {
             </Link>
 
             <Link
-              to="home"
+              to="reviews"
               spy={true}
               smooth={true}
               duration={500}
@@ -54,9 +66,55 @@ const Navbar = () => {
             >
               Reviews
             </Link>
-            
-            <Button title="Login"/>
+
+            <Button title="Login" />
           </nav>
+
+          <div className="md:hidden flex items-center">
+            {menu ? (
+              <AiOutlineClose size={25} onClick={handleChange} />
+            ) : (
+              <AiOutlineMenuFold size={25} onClick={handleChange} />
+            )}
+          </div>
+        </div>
+        <div
+          className={` ${
+            menu ? "translate-x-0" : "-translate-x-full"
+          } lg:hidden flex flex-col absolute bg-black text-white left-0 top-20 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}
+        >
+
+            <Link
+              to="home"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className=" hover:text-blue-400 transition-all cursor-pointer"
+            >
+              Home
+            </Link>
+
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className=" hover:text-blue-400 transition-all cursor-pointer"
+            >
+              About
+            </Link>
+
+            <Link
+              to="reviews"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className=" hover:text-blue-400 transition-all cursor-pointer"
+            >
+              Reviews
+            </Link>
+
+            <Button title="login" />
         </div>
       </div>
     </div>
