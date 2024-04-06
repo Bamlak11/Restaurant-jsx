2dddfcb6b4f3d9d1e2e216cf10f85c1db7b2803c
Bamlak11 <77420100+Bamlak11@users.noreply.github.com>
2024-04-06 17:56:09 -0500

    creating home layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Button from './Button'
 
 const Home = () => {
   return (
-    <div>
-      <div>
-        <h1>ExpressEats</h1>
+    <div className=" min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('https://images.pexels.com/photos/6059778/pexels-photo-6059778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-cover bg-no-repeat ">
+      <div className='w-full lg:w-2/3 space-y-5'>
+        <h1 className=" text-backgroundColor font-semibold text-6xl">ExpressEats</h1><br></br>
+        <p className=" text-backgroundColor">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+
+        <div>
+            <Button title="Order Now"/>
+        </div>
       </div>
     </div>
   )
