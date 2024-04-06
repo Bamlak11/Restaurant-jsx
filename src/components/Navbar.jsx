dcca2b0d56f9c10b5a95123bcbed5336d91baf28
Bamlak11 <77420100+Bamlak11@users.noreply.github.com>
2024-04-06 17:11:42 -0500

    styled nav and created a button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import { BiRestaurant } from "react-icons/bi";
 import { Link } from "react-scroll";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className=" fixed w-full">
       <div>
-        <div>
-          <div>
-            <span></span>
+        <div className=" flex flex-row justify-between p-5 md:px-32 px-5 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+          <div className=" flex flex-row items-center cursor-pointer">
+            <span>
+              <BiRestaurant size={32}/>
+            </span>
             <h1>ExpressEats</h1>
           </div>
-          <nav>
+          <nav className="hidden md:flex flex-row items-center text-lg font-medium gap-8">
           <Link
               to="home"
               spy={true}
               smooth={true}
               duration={500}
-              className="hover:text-brightColor transition-all cursor-pointer"
+              className=" hover:text-blue-400 transition-all cursor-pointer"
             >
               Home
             </Link>
@@ -26,7 +30,7 @@ const Navbar = () => {
               spy={true}
               smooth={true}
               duration={500}
-              className=" cursor-pointer"
+              className=" hover:text-blue-400 transition-all cursor-pointer"
             >
               Menu
             </Link>
@@ -36,7 +40,7 @@ const Navbar = () => {
               spy={true}
               smooth={true}
               duration={500}
-              className=" cursor-pointer"
+              className=" hover:text-blue-400 transition-all cursor-pointer"
             >
               About
             </Link>
@@ -46,11 +50,12 @@ const Navbar = () => {
               spy={true}
               smooth={true}
               duration={500}
-              className=" cursor-pointer"
+              className=" hover:text-blue-400 transition-all cursor-pointer"
             >
               Reviews
             </Link>
             
+            <Button title="Login"/>
           </nav>
         </div>
       </div>
