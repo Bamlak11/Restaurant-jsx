d23b6bd34ec10edd1468fb0f1ebf136fae9b4f59
Bamlak11 <77420100+Bamlak11@users.noreply.github.com>
2024-04-06 22:12:49 -0500

    done with about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import Button from './Button'
 
 const About = () => {
   return (
     <div className="min-h-screen flex flex-col lg:flex-row justify-center items-center lg:px-32 px-5">
-      <img src="https://images.pexels.com/photos/5638730/pexels-photo-5638730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="About Image" />
+      <img className=' rounded-3xl mb-8 lg:mb-0 lg:mr-8' src="https://images.pexels.com/photos/5638730/pexels-photo-5638730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="About Image" width={700} />
 
       <div className='space-y-4 lg:pt-14'>
         <h1 className=" font-semibold text-4xl text-center md:text-start">Why ExpressEats?</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          architecto quisquam delectus minima perferendis nulla quia nisi
+          laborum, exercitationem cum quo accusantium, impedit sed. Dicta, quo
+          molestias omnis reprehenderit quae animi?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi,
+          suscipit reiciendis accusamus recusandae eum aspernatur pariatur odit
+          veritatis facere. Magnam!
+        </p>
+        <div className=" flex justify-center lg:justify-start">
+            <Button title="Learn More"/>
+        </div>
       </div>
     </div>
 
--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -5,7 +5,7 @@ import Button from './Button';
 const MenuCard = (props) => {
   return (
     <div className=" w-full lg:w-1/4 p-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] rounded-lg">
-      <img className=" rounded-xl" src={props.img} alt="image" />
+      <img className=" rounded-xl duration-300 hover:scale-105 " src={props.img} alt="image" />
       <div className=" space-y-4">
         <h3 className=" font-semibold text-center text-xl pt-6">{props.title}</h3>
         <div className=" flex flex-row justify-center">
